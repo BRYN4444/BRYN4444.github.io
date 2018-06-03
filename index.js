c244ca9f62455c7c7eccffd34ed4c685720879e1
BRYN <bryn4444@gmail.com>
2018-06-03 17:36:35 -0600

    Added favicon and random header, updated style.

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var quotes = [
+		"Dabbles in a little bit of everything.",
+		"Be cool. Just once, be cool.",
+		"So, this is how you're spending your time?"
+	];
+    randno = quotes[Math.floor( Math.random() * quotes.length )];
+    $('h2').text( randno );
+});
