ba0b1f183ee698420912b0cfd6b7be49919f8b45
BRYN <bryn4444@gmail.com>
2018-06-03 17:40:36 -0600

    Attempt to fix random header code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-    var quotes = [
-		"Dabbles in a little bit of everything.",
-		"Be cool. Just once, be cool.",
-		"So, this is how you're spending your time?"
-	];
+    var quotes = new Array("Dabbles in a little bit of everything.", "Be cool. Just once, be cool.", "So, this is how you're spending your time?"),
     randno = quotes[Math.floor( Math.random() * quotes.length )];
     $('h2').text( randno );
 });
