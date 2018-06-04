bdea9bfa09df0975765c08ec718db316da85db59
BRYN <bryn4444@gmail.com>
2018-06-03 22:00:27 -0600

    Fix CSS transition, Added info section.

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ $(document).ready(function() {
     randno = quotes[Math.floor( Math.random() * quotes.length )];
     $('h2').text( randno );
 	
-	var thequote;    
-	$('a').hover(function(){
+	var oldtext;    
+	$('a#b, a#r, a#y, a#n, a#dot').hover(function(){
 		linkurl = $(this).attr('href');
 		linktext = linkurl.replace(/^https?:\/\//,'')
 		oldtext = $('h2').text();
 		$('h2').text(linktext);
+	}, function(){
+		$('h2').text(oldtext)
+	});
+	$('a#info').hover(function(){
+		infotext = $(this).attr('data-about');
+		oldtext = $('h2').text();
+		$('h2').text(infotext);
 	}, function(){
 		$('h2').text(oldtext)
 	});
