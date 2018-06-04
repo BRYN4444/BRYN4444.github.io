0ab555d59c2d6e9733b2b5c566a448ee71ac865d
BRYN <bryn4444@gmail.com>
2018-06-03 19:09:28 -0600

    CSS update, URL text script added

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
     var quotes = new Array("Dabbles in a little bit of everything.", "Be cool. Just once, be cool.", "So, this is how you're spending your time?"),
     randno = quotes[Math.floor( Math.random() * quotes.length )];
     $('h2').text( randno );
+	
+	var thequote;    
+	$('a').hover(function(){
+		oldtext = $(this).text();
+		linktext = $(this).attr('href');
+		$(this).text(linktext);
+	}, function(){
+		$(this).text(oldtext)
+	});
 });
