f33e50f5ac6a2f062294f39048dc2b9b50c75542
BRYN <bryn4444@gmail.com>
2024-11-03 15:56:09 -0700

    update

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 $(document).ready(function() {
-    var quotes = new Array("Dabbles in a little bit of everything.", "Be cool. Just once, be cool.", "So, this is how you're spending your time?","Yes, the font is hand-made. Thanks for asking.","Proud supporter of dark themes for every website.","Your redirection destination."),
-    randno = quotes[Math.floor( Math.random() * quotes.length )];
-    $('h2').text( randno );
 	
-	var oldtext;    
-	$('a#b, a#r, a#y, a#n, a#dot').hover(function(){
-		linkurl = $(this).attr('href');
-		linktext = linkurl.replace(/^https?:\/\//,'')
-		oldtext = $('h2').text();
-		$('h2').text(linktext);
-	}, function(){
-		$('h2').text(oldtext)
+	/*Random Text*/
+	var quotes = ["Generally inimportant.",
+	"Be cool. Just once, be cool.",
+	"Proud supporter of dark themes for every website.",
+	"Your Ad Here!",
+	"Your redirection destination."];
+    var index = 0;
+
+    function updateText() {
+        $('h2').fadeOut(500, function() {
+            $(this).text(quotes[index]).fadeIn(500);
+            index = (index + 1) % quotes.length;
+        });
+    }
+	
+    updateText(); // Initial call
+    setInterval(updateText, 5000); // Update interval
+	
+	
+	/*Text Color*/
+	$( "a#bsky" ).hover(function() {
+		$( "h1" ).addClass('blue');
+	}, function() {
+		$( "h1" ).removeClass('blue');
 	});
-	$('a#info').hover(function(){
-		infotext = $(this).attr('data-about');
-		oldtext = $('h2').text();
-		$('h2').text(infotext);
-	}, function(){
-		$('h2').text(oldtext)
+	$( "a#youtube" ).hover(function() {
+		$( "h1" ).addClass('red');
+	}, function() {
+		$( "h1" ).removeClass('red');
 	});
+	$( "a#twitch" ).hover(function() {
+		$( "h1" ).addClass('purple');
+	}, function() {
+		$( "h1" ).removeClass('purple');
+	});
+	
 });
